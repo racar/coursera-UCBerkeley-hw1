59f216c3d43c8e08a7a282a51debf0a13d7b6164
Rafael Carrascal Reyes <rcarrascal@gmail.com>
2012-03-01 06:12:31 -0500

    more cleaning

--- RockPaperScissorsTest.rb
+++ RockPaperScissorsTest.rb
@@ -3,27 +3,27 @@ require './RockPaperScissors.rb'
 class RockPaperScissorsTest < Test::Unit::TestCase 
   
   def test_rps_game_winner
-    rockPaperScissors = RockPaperScissors.new
     
-    assert_equal ["Dave", "S"],rockPaperScissors.rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ])
-    assert_equal ["Dave", "P"],rockPaperScissors.rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ] ])
-    assert_equal ["Armando", "R"],rockPaperScissors.rps_game_winner([ [ "Armando", "R" ], [ "Dave", "R" ] ])
     
-    assert_raise(WrongNumberOfPlayersError) {rockPaperScissors.rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ],["Rafael","S"] ])} 
-    assert_raise(WrongNumberOfPlayersError) {rockPaperScissors.rps_game_winner([ [ "Armando", "R" ]])}
+    assert_equal ["Dave", "S"],rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ])
+    assert_equal ["Dave", "P"],rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ] ])
+    assert_equal ["Armando", "R"],rps_game_winner([ [ "Armando", "R" ], [ "Dave", "R" ] ])
+    
+    assert_raise(WrongNumberOfPlayersError) {rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ],["Rafael","S"] ])} 
+    assert_raise(WrongNumberOfPlayersError) {rps_game_winner([ [ "Armando", "R" ]])}
    
-    assert_raise(NoSuchStrategyError) {rockPaperScissors.rps_game_winner([ [ "Armando", "X" ], [ "Dave", "P" ]])}
+    assert_raise(NoSuchStrategyError) {rps_game_winner([ [ "Armando", "X" ], [ "Dave", "P" ]])}
    
     
     
   end
   
   def test_rps_tournament_winner
-    rockPaperScissors = RockPaperScissors.new
     
-   assert_equal ["Dave", "S"],rockPaperScissors.rps_tournament_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ])
+    
+   assert_equal ["Dave", "S"],rps_tournament_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ])
    
-   assert_equal ["Richard", "R"],rockPaperScissors.rps_tournament_winner([
+   assert_equal ["Richard", "R"],rps_tournament_winner([
        [
       [["Armando", "P"], ["Dave", "S"] ],
       [ ["Richard", "R"], ["Michael", "S"], ]
@@ -31,8 +31,7 @@ class RockPaperScissorsTest < Test::Unit::TestCase
      ])
    end
    def test_ocho_players 
-      rockPaperScissors = RockPaperScissors.new
-    assert_equal ["Richard", "R"],rockPaperScissors.rps_tournament_winner([ 
+          assert_equal ["Richard", "R"],rps_tournament_winner([ 
       [
       [ ["Armando", "P"], ["Dave", "S"] ],
       [ ["Richard", "R"], ["Michael", "S"] ], ],
@@ -44,8 +43,8 @@ class RockPaperScissorsTest < Test::Unit::TestCase
  end
   
     def test_diezseis_players 
-  rockPaperScissors = RockPaperScissors.new
- assert_equal ["A3", "R"],rockPaperScissors.rps_tournament_winner(
+  
+ assert_equal ["A3", "R"],rps_tournament_winner(
  [
   [
     [
@@ -74,8 +73,8 @@ class RockPaperScissorsTest < Test::Unit::TestCase
   end
   
   def test_treintados_players 
-  rockPaperScissors = RockPaperScissors.new
- assert_equal ["A3", "R"],rockPaperScissors.rps_tournament_winner(
+  
+ assert_equal ["A3", "R"],rps_tournament_winner(
  [
   [
       [
